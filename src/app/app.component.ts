45fcc5413d243f60bacf4743e2b487f598d2d35b
Fabiano <fabiano.ssantos79@gmail.com>
2024-09-29 03:06:56 -0300

    feat: update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from "./components/form/form.component";
+import { CardComponent } from "./components/card/card.component";
+import { ContainerComponent } from "./components/container/container.component";
+import { HttpClientModule } from '@angular/common/http';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormComponent, CardComponent, ContainerComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'search-github';
+export class AppComponent implements OnInit{
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void{
+    this.userService.getUser('facebook').subscribe((user) => console.log(user)
+    )
+  }
+
+
 }
